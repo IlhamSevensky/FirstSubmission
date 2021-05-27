f8b2f13d44f53a494e9b131bdefff4ab63cdb833
Ilham Budi Prasetyo <budip.ilham@gmail.com>
2021-05-28 04:45:27 +0700

    add circleci to master

--- shared_dependencies.gradle.kts
+++ shared_dependencies.gradle.kts
@@ -29,9 +29,9 @@ dependencies {
     "kapt"(Dependencies.Hilt.COMPILER)
     "implementation"(Dependencies.FlowBinding.ANDROID)
     "implementation"(Dependencies.FlowBinding.APPCOMPAT)
+    "implementation"(Dependencies.TIMBER)
     "implementation"("com.google.android.play:core:1.8.3")
     "implementation"("com.google.android.play:core-ktx:1.8.1")
-    "implementation"(Dependencies.TIMBER)
 }
 
 configurations {
